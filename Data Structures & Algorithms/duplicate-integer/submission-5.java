084a96d3ebd2b57faf0e467a1d9c41ebcfe88e7f
Mohit Gujarati <95397404+MohitGujarati@users.noreply.github.com>
2026-09-04 07:42:34 -0700

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+
+      for (int num : nums) {
+        //if we dont add the add number it means its already present 
+           if(!seen.add(num)){
+            return true;
+           }
+        }
+
+        return false;
+    }
+}
+
+
+
+
